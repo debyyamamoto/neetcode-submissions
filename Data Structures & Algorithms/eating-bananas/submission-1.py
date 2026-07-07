d4896ce7b0ac6941b5d9b5eddf53cbebc98fef64
Déborah Yamamoto <119982741+debyyamamoto@users.noreply.github.com>
2026-07-07 16:45:33 -0300

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,29 @@
+from typing import List
+
+# qual é o menor valor de rating de comida que me possibilita comer todas as bananas
+# no tempo h
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def pode(k):
+            # quantas horas o macaco comeria a pilha dado esse rating k?
+            horas = 0
+            for bananas in piles:
+                resto = bananas % k  
+                horas += (bananas//k)
+                if resto != 0:
+                    horas += 1
+                
+            if horas <= h:
+                return True
+            else:
+                return False
+        lo = 1
+        hi = max(piles)
+        while lo < hi:
+            mid = lo + (hi-lo) // 2
+            if pode(mid):
+                hi = mid
+            else:
+                lo = mid + 1
+        return lo
+
